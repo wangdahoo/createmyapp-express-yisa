e4f9978841e1101e49ef33a61c9ae1ebd2535dcd
wangdahoo <157195705@qq.com>
2017-11-29 21:18:26 +0800

    add nodemon and move `public`, `views` into `app`

--- app/index.js
+++ app/index.js
@@ -11,7 +11,7 @@ const user = require('./routes/user')
 const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'))
+app.set('views', path.join(__dirname, './views'))
 app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
@@ -23,10 +23,12 @@ app.use(cookieParser())
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
+  outputStyle: 'compressed',
   indentedSyntax: true,
-  sourceMap: true
+  sourceMap: true,
+  prefix: '/static'
 }))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.use('/', index)
 app.use('/user', user)
